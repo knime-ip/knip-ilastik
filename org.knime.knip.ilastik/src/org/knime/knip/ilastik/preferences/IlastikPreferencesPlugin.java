f3d626778b35ced3b8b1579cf2019c43cb19132a
Gabriel Einsdorf <gabriel.einsdorf@uni-konstanz.de>
2017-11-16 18:35:58 +0100

    Set the ilastik settings to the IlastikOptions on plugin activation

--- org.knime.knip.ilastik/src/org/knime/knip/ilastik/preferences/IlastikPreferencesPlugin.java
+++ org.knime.knip.ilastik/src/org/knime/knip/ilastik/preferences/IlastikPreferencesPlugin.java
@@ -1,6 +1,12 @@
 package org.knime.knip.ilastik.preferences;
 
+import java.io.File;
+
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.ilastik.ilastik4ij.IlastikOptions;
+import org.knime.knip.imagej2.core.IJGateway;
+import org.scijava.options.OptionsService;
 
 /**
  * Plugin instance for the Ilastik integration
@@ -11,5 +17,27 @@ public class IlastikPreferencesPlugin extends AbstractUIPlugin {
 
     public static IlastikPreferencesPlugin getDefault() {
         return instance;
+    }
+
+    public IlastikPreferencesPlugin() {
+        OptionsService optionsService = IJGateway.getImageJContext().getService(OptionsService.class);
+        if (optionsService == null) {
+            throw new IllegalStateException("Can not load options service");
+        }
+
+        final IlastikOptions ilastikOptions = optionsService.getOptions(IlastikOptions.class);
+
+        if (ilastikOptions == null) {
+            throw new IllegalStateException("Can not load IlastikOptions service");
+        }
+
+        final IPreferenceStore preferenceStore = getPreferenceStore();
+        ilastikOptions.setExecutableFilePath(preferenceStore.getString(IlastikPreferenceConstants.P_PATH));
+        ilastikOptions.setMaxRamMb(preferenceStore.getInt(IlastikPreferenceConstants.P_RAM));
+        ilastikOptions.setNumThreads(preferenceStore.getInt(IlastikPreferenceConstants.P_THREADS));
+
+        if (!new File(ilastikOptions.getExecutableFilePath()).exists()) {
+            throw new IllegalStateException("Selected file is not a valid ilastik executable");
+        }
     }
 }
